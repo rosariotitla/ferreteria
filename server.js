d5d37b16c30b8b11b0cf760adc9d68a98629dea0
rosariotitla <rosario.titla@utpuebla.edu.mx>
2016-06-30 18:23:08 -0500

    inventario
    ferreteria

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 var path = require('path');
 var rootPath = path.normalize(__dirname);
 var nodePort = '2009';
-
+var favicon=require('serve-favicon');
 app.use(express.static(rootPath));
 //console.log(rootPath);
 app.use('/node_modules', express.static(rootPath + '/node_modules'))
